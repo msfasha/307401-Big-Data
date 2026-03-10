8e4cef96742071b2fea45db6ff281b2425e3da0b
Mohammad Fasha <mohd.fasha@gmail.com>
2026-03-10 16:16:51 +0300

    enhance single vs parallel example, display time durations and verbose messages

--- 20252/Module 2 - Introduction to Hadoop/Parallel Processing Example/single_vs_multi-threads.py
+++ 20252/Module 2 - Introduction to Hadoop/Parallel Processing Example/single_vs_multi-threads.py
@@ -2,6 +2,7 @@ import os
 from multiprocessing import Pool, cpu_count
 import numpy as np
 import time
+from datetime import datetime, timedelta
 
 # Function to count IDs in a chunk (used in both single and multi-process)
 def count_chunk(chunk):
@@ -45,20 +46,29 @@ def count_ids_multiprocess(customer_ids):
 
 if __name__ == "__main__":
     # Step 1: Generate a random array of 50 million customer IDs between 1 and 10000
+    print("Generating 50,000,000 random customer IDs...\n")
     customer_ids = np.random.randint(1, 10001, size=50_000_000)
 
     # Measure the time for single-process counting
     start_time_single = time.time()
+    print("Counting customer IDs using single process...")
+    print(f"start_time_single: {datetime.fromtimestamp(start_time_single):%H:%M:%S}")
     single_process_result = count_ids_single_process(customer_ids)
     end_time_single = time.time()
     single_process_duration = end_time_single - start_time_single
+    print(f"end_time_single: {datetime.fromtimestamp(end_time_single):%H:%M:%S}")
+    print(f"single_process_duration: {timedelta(seconds=single_process_duration)}\n\n")
+
 
     # Measure the time for multi-process counting
+    print("Counting customer IDs using multi-process...")    
     start_time_multi = time.time()
+    print(f"start_time_multi: {datetime.fromtimestamp(start_time_multi):%H:%M:%S}")
     multi_process_result = count_ids_multiprocess(customer_ids)
     end_time_multi = time.time()
-    multi_process_duration = end_time_multi - start_time_multi
+    print(f"end_time_multi: {datetime.fromtimestamp(end_time_multi):%H:%M:%S}")
 
-    # Print the results
-    print(f"Single-process duration: {single_process_duration} seconds")
-    print(f"Multi-process duration (using {cpu_count()} cores): {multi_process_duration} seconds")
+    multi_process_duration = end_time_multi - start_time_multi
+    print(f"Multi-process duration (using {cpu_count()} cores): {timedelta(seconds=multi_process_duration)}\n")
+
+    
